8e5677b4b7637f0fca32a7d2caf86eadfe706cd5
Binod Bajgai <bajgaibenod@gmail.com>
2026-09-08 22:05:57 +0545

    Fix Flask deployment configuration

--- app.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from app import create_app
-
-app = create_app()
-
-if __name__ == "__main__":
-    debug_mode = os.getenv("FLASK_DEBUG", "False") == "True"
-    app.run(debug=debug_mode)
